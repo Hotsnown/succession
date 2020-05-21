b99434ff9a5fd9077cb176416b4447ce1ff87640
Pierre Deboissy <deboissy.pierre@gmail.com>
2020-05-21 09:32:26 +0200

    add client react tree component

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Tree from './client/Tree/Tree';
-import * as serviceWorker from './serviceWorker';
-
+import Tree from './Tree/Tree';
+import registerServiceWorker from './registerServiceWorker';
 
 var simpsonsTree = {
   abe: {
@@ -42,7 +41,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
