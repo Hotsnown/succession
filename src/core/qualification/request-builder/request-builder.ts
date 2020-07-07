b55bda5994beec6ce14380b4221ac51df345f4c4
Pierre Deboissy <deboissy.pierre@gmail.com>
2020-07-07 10:32:38 +0200

    add qualification type

--- src/core/qualification/request-builder/request-builder.ts
+++ src/core/qualification/request-builder/request-builder.ts
@@ -1,7 +1,11 @@
 import { Query } from './interface';
 const fetch = require('node-fetch')
 
-async function getQualificationFrom (query: Query) {
+interface QualificationResult {
+  task: any
+}
+
+async function getQualificationFrom (query: Query): Promise<QualificationResult> {
 
     const requestOptions = {
       method: 'POST',
@@ -9,12 +13,11 @@ async function getQualificationFrom (query: Query) {
       body: JSON.stringify(query)
     };
     
-    const result  = await fetch("http://127.0.0.1:5000/todo/api/v1.0/tasks", requestOptions)
+    const result: QualificationResult = await fetch("http://127.0.0.1:5000/todo/api/v1.0/tasks", requestOptions)
       .then((response: Response) => response.json())
       .then((result: string) => {return result})
-      .catch(() => alert('Server has not been found. Please set it up with "npm run python".'))
-    ;
-
+      .catch(() => alert('Server has not been found. Please set it up with "npm run python".'));
+      
     return result
 }
 
