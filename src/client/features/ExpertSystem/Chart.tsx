9b14399e58ffd5ba9329c302edfc1899901dd7d3
Pierre Deboissy <deboissy.pierre@gmail.com>
2020-09-25 15:11:41 +0200

    remove old tree renderer

--- src/client/features/ExpertSystem/Chart.tsx
+++ src/client/features/ExpertSystem/Chart.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-
-import Tree from './Tree/Tree'
-import { App } from './Tree2/App'
-import initialTreeValue from './initialTreeValue'
+import { Tree } from './Tree/Tree'
 import { RouteComponentProps } from 'react-router-dom'
 
 interface ChartProps {
@@ -16,23 +13,13 @@ interface ChartProps {
 }
 
 export const Chart = (props: ChartProps & RouteComponentProps) => {
-    if (props.renderTree) {
-        return (
-            <Tree
-                root={props.root}
-                datalist={JSON.parse(props.family) || JSON.parse(JSON.stringify(initialTreeValue))}
-                onUpdateDeCujus={props.handleUpdateDeCujus}
-                onUpdateMemberList={props.onUpdateMemberList} />
-        )
-    } else {
-        return (
-            <App 
-                {...props}
-                onUpdateMemberList={props.onUpdateMemberList}
-                onUpdateDeCujus={props.handleUpdateDeCujus}
-                deCujus={props.deCujus}
-                processSolution={props.processSolution}
-            ></App>
-        )
-    }
+    return (
+        <Tree 
+            {...props}
+            onUpdateMemberList={props.onUpdateMemberList}
+            onUpdateDeCujus={props.handleUpdateDeCujus}
+            deCujus={props.deCujus}
+            processSolution={props.processSolution}
+        ></Tree>
+    )
 }
--- src/client/features/ExpertSystem/Tree/Tree.tsx
+++ src/client/features/ExpertSystem/Tree/Tree.tsx
@@ -1,143 +1,413 @@
-/*eslint-disable*/
-import React from 'react';
-import TreeMember from './TreeMember/TreeMember';
-import TreeParser from './TreeParser';
+import * as H from 'history';
+import * as queryString from 'query-string';
+import * as React from 'react';
+import {analyticsEvent} from '../../../utils/analytics';
+import {Chart, ChartType} from './Chart';
+import {Details} from './details';
+import {EmbeddedDataSource, EmbeddedSourceSpec} from './datasources/embedded';
+import {TopolaData} from '../../../utils/gedcom-utils';
+import {IndiInfo} from 'topola';
+import {Loader, Responsive} from 'semantic-ui-react';
+import {TopBar} from './menu/topbar';
+import {DataSourceEnum, SourceSelection} from './datasources/data_source';
+import {
+  getSelection,
+  UploadSourceSpec,
+  UrlSourceSpec,
+  GedcomUrlDataSource,
+  UploadedDataSource,
+} from './datasources/load_data';
+import { ErrorPopup, ErrorMessage } from './ErrorMessage';
+import { data } from './datasources2/Ordre1'
+import { RouteComponentProps } from 'react-router-dom';
 
-interface TreeProps {
-  root: string
-  datalist: any
-  onUpdateDeCujus: (value: string) => void
-  onUpdateMemberList: (memberlist: any) => void
+enum TreeState {
+  INITIAL,
+  LOADING,
+  ERROR,
+  SHOWING_CHART,
+  LOADING_MORE,
 }
 
-interface TreeState {
-  memberlist: any
-  rootid: string
-  membercount: number
-  position?: any
+type DataSourceSpec =
+  | UrlSourceSpec
+  | UploadSourceSpec
+  | EmbeddedSourceSpec;
+
+/** Arguments passed to the application, primarily through URL parameters. */
+interface Arguments {
+  sourceSpec?: DataSourceSpec;
+  selection?: IndiInfo;
+  chartType: ChartType;
+  standalone: boolean;
+  freezeAnimation?: boolean;
+  showSidePanel: boolean;
 }
 
-class Tree extends React.Component <TreeProps, TreeState> {
+/**
+ * Retrieve arguments passed into the application through the URL and uploaded
+ * data.
+ */
+function getArguments(location: H.Location<any>): Arguments {
+  const search = queryString.parse(location.search);
+  const getParam = (name: string) => {
+    const value = search[name];
+    return typeof value === 'string' ? value : undefined;
+  };
 
-  memberlist: Tree[]
-  nextMemberId: number
+  const view = getParam('view');
+  const chartTypes = new Map<string | undefined, ChartType>([
+    ['relatives', ChartType.Relatives],
+    ['fancy', ChartType.Fancy],
+  ]);
 
-  constructor(props: TreeProps) {
-    super(props);
-
-    // normalize all possible tree representations into single nested data object
-    var parser = new TreeParser(props.datalist, props.root);
-    this.memberlist = parser.getMemberlist();
-    this.nextMemberId = parser.getNextMemberId();
-    this.state = {
-      memberlist: this.memberlist,
-      rootid: this.props.root,
-      membercount: this.memberlist.length//parseInt(this.memberlist.length)
-    }
-
-    this.handleAddPartner = this.handleAddPartner.bind(this);
-    this.handleAddChild = this.handleAddChild.bind(this);
-    this.handleMemberEdit = this.handleMemberEdit.bind(this);
-    this.handleMemberDelete = this.handleMemberDelete.bind(this);
-    this.handleUpdateStatus = this.handleUpdateStatus.bind(this);
+  const hash = getParam('file');
+  const url = getParam('url');
+  const embedded = getParam('embedded') === 'true'; // False by default.
+  var sourceSpec: DataSourceSpec | undefined = undefined;
+  if (hash) {
+    sourceSpec = {
+      source: DataSourceEnum.UPLOADED,
+      hash,
+      gedcom: location.state && location.state.data,
+      images: location.state && location.state.images,
+    };
+  } else if (url) {
+    sourceSpec = {
+      source: DataSourceEnum.GEDCOM_URL,
+      url,
+      handleCors: getParam('handleCors') !== 'false', // True by default.
+    };
+  } else if (embedded) {
+    sourceSpec = {source: DataSourceEnum.EMBEDDED};
   }
 
-  componentDidUpdate(prev, props) {
-    this.props.onUpdateMemberList(this.state.memberlist)
+  const indi = getParam('indi');
+  const parsedGen = Number(getParam('gen'));
+  const selection = indi
+    ? {id: indi, generation: !isNaN(parsedGen) ? parsedGen : 0}
+    : undefined;
+
+  return {
+    sourceSpec,
+    selection,
+    // Hourglass is the default view.
+    chartType: chartTypes.get(view) || ChartType.Hourglass,
+
+    showSidePanel: getParam('sidePanel') !== 'false', // True by default.
+    standalone: getParam('standalone') !== 'false' && !embedded,
+    freezeAnimation: getParam('freeze') === 'true', // False by default
+  };
+}
+
+/**
+ * Returs true if the changes object has values that are different than those
+ * in state.
+ */
+function hasUpdatedValues<T>(state: T, changes: Partial<T> | undefined) {
+  if (!changes) {
+    return false;
+  }
+  return Object.entries(changes).some(
+    ([key, value]) => value !== undefined && state[key] !== value,
+  );
+}
+
+interface State {
+  state: TreeState;
+  data?: TopolaData;
+  selection?: IndiInfo;
+  error?: string;
+  showSidePanel?: boolean;
+  standalone: boolean;
+  chartType: ChartType;
+  showErrorPopup: boolean;
+  sourceSpec?: DataSourceSpec;
+  freezeAnimation?: boolean;
+}
+
+interface TreeProps {
+  onUpdateDeCujus: (value: string) => void;
+  onUpdateMemberList: (memberList: any) => void
+  deCujus: string
+  processSolution: () => void
+}
+
+export class Tree extends React.Component<RouteComponentProps & TreeProps, State> {
+
+  state: State = {
+    state: TreeState.INITIAL,
+    standalone: true,
+    chartType: ChartType.Hourglass,
+    showErrorPopup: false,
+  };
+  chartRef: Chart | null = null;
+
+  /** Sets the state with a new individual selection and chart type. */
+  private updateDisplay(
+    selection: IndiInfo,
+    otherStateChanges?: Partial<State>,
+  ) {
+    if (
+      !this.state.selection ||
+      this.state.selection.id !== selection.id ||
+      this.state.selection!.generation !== selection.generation ||
+      hasUpdatedValues(this.state, otherStateChanges)
+    ) {
+      this.setState(
+        Object.assign({}, this.state, {selection}, otherStateChanges),
+      );
+    }
+  }
+
+  /** Sets error message after data load failure. */
+  private setError(error: string) {
+    this.setState(
+      Object.assign({}, this.state, {
+        state: TreeState.ERROR,
+        error,
+      }),
+    );
   }
 
   componentDidMount() {
-    this.props.onUpdateMemberList(this.state.memberlist)
+    this.componentDidUpdate();
   }
 
-  getNextMemberId(memberlist: undefined | any[]) {
-    if (memberlist === undefined) memberlist = this.state.memberlist;
-    //@ts-ignore
-    let ids = Object.keys(memberlist);
-    let append = 1;
-    let prepend = 'new_member_';
-    while (ids.indexOf(prepend + append) >= 0) append++;
-    return prepend + append;
+  private readonly uploadedDataSource = new UploadedDataSource();
+  private readonly gedcomUrlDataSource = new GedcomUrlDataSource();
+  private readonly embeddedDataSource = new EmbeddedDataSource();
+
+  private isNewData(sourceSpec: DataSourceSpec, selection?: IndiInfo) {
+    if (
+      !this.state.sourceSpec ||
+      this.state.sourceSpec.source !== sourceSpec.source
+    ) {
+      // New data source means new data.
+      return true;
+    }
+    const newSource = {spec: sourceSpec, selection};
+    const oldSouce = {
+      spec: this.state.sourceSpec,
+      selection: this.state.selection,
+    };
+    switch (newSource.spec.source) {
+      case DataSourceEnum.UPLOADED:
+        return this.uploadedDataSource.isNewData(
+          newSource as SourceSelection<UploadSourceSpec>,
+          oldSouce as SourceSelection<UploadSourceSpec>,
+          this.state.data,
+        );
+      case DataSourceEnum.GEDCOM_URL:
+        return this.gedcomUrlDataSource.isNewData(
+          newSource as SourceSelection<UrlSourceSpec>,
+          oldSouce as SourceSelection<UrlSourceSpec>,
+          this.state.data,
+        );
+      case DataSourceEnum.EMBEDDED:
+        return this.embeddedDataSource.isNewData(
+          newSource as SourceSelection<EmbeddedSourceSpec>,
+          oldSouce as SourceSelection<EmbeddedSourceSpec>,
+          this.state.data,
+        );
+    }
   }
 
-  getNewMember(name: string, id: string) {
-    //@ts-ignore
-    return { 'id': id, 'name': name, 'partners': [], 'children': [] }
+  private loadData(sourceSpec: DataSourceSpec, selection?: IndiInfo) {
+    switch (sourceSpec.source) {
+      case DataSourceEnum.UPLOADED:
+        return this.uploadedDataSource.loadData({spec: sourceSpec, selection});
+      case DataSourceEnum.GEDCOM_URL:
+        return this.gedcomUrlDataSource.loadData({spec: sourceSpec, selection});
+      case DataSourceEnum.EMBEDDED:
+        return this.embeddedDataSource.loadData({spec: sourceSpec, selection});
+    }
   }
 
-  handleMemberEdit(member_id: string, data: any) {
-    this.setState(function (prev_state, props) {
-      var memberlist = { ...prev_state.memberlist };
-      memberlist[member_id].name = data.name;
-      return { 'memberlist': memberlist };
-    });
-  }
+  async componentDidUpdate() {
 
-  handleUpdateStatus(member_id: string) {
-    this.setState(function (prev_state, props) {
-      let memberlist = { ...prev_state.memberlist };
-      memberlist[member_id].status = memberlist[member_id].status === 'valid' ? 'invalid' : 'valid'
-      return { 'memberlist': memberlist };
-    });
-  }
-
-  handleMemberDelete(member_id: string) {
-    this.setState(function (prev_state, props) {
-      var memberlist = { ...prev_state.memberlist };
-      var member = memberlist[member_id];
-      if (member.partners != null && member.partners.length > 0) alert('On ne peut pas supprimer un membre ayant un partenaire');
-      else alert("Il n\' est pas encore possible de supprimer");
-    });
-  }
-
-  handleAddPartner(root_id: string) {
-    this.setState(function (prev_state, props) {
-      let memberlist = { ...prev_state.memberlist };
-      //@ts-ignore
-      let new_id = this.getNextMemberId(memberlist);
-      //@ts-ignore
-      let new_member = this.getNewMember('Nouveau partenaire', new_id);
-      memberlist[new_member.id] = new_member;
-      memberlist[root_id].partners.push(new_member);
-      memberlist[root_id].children[new_member.id] = [];
-      return { 'memberlist': memberlist };
-    });
-  }
-
-  handleAddChild(root_id: string, partner_id: string) {
-    this.setState(function (prev_state, props) {
-      let memberlist = { ...prev_state.memberlist };
-      //@ts-ignore
-      let new_id = this.getNextMemberId(memberlist);
-      //@ts-ignore
-      let new_member = this.getNewMember('Nouvel enfant', new_id);
-      memberlist[new_member.id] = new_member;
-      if (!Array.isArray(memberlist[root_id].children[partner_id])) {
-        memberlist[root_id].children[partner_id] = [];
+    if (this.props.location.pathname !== '/succession/dashboard/view') {
+      if (this.state.state !== TreeState.INITIAL) {
+        this.setState(Object.assign({}, this.state, {state: TreeState.INITIAL}));
       }
-      memberlist[root_id].children[partner_id].push(new_member);
-      return { 'memberlist': memberlist };
-    });
+      return;
+    }
+
+    const args = getArguments(this.props.location);
+
+    if (!args.sourceSpec) {
+      this.props.history.replace({pathname: '/'});
+    } else if (
+      this.state.state === TreeState.INITIAL ||
+      this.isNewData(args.sourceSpec, args.selection)
+    ) {
+      // Set loading state.
+      this.setState(
+        Object.assign({}, this.state, {
+          state: TreeState.LOADING,
+          sourceSpec: args.sourceSpec,
+          selection: args.selection,
+          standalone: args.standalone,
+          chartType: args.chartType,
+        }),
+      );
+      try {
+        const data = await this.loadData(args.sourceSpec, args.selection);
+        console.log(data)
+        // Set state with data.
+        this.setState(
+          Object.assign({}, this.state, {
+            state: TreeState.SHOWING_CHART,
+            data,
+            selection: getSelection(data.chartData, args.selection),
+          }),
+        );
+      } catch (error) {
+        this.setError(error.message);
+      }
+    }
   }
+
+  /**
+   * Called when the user clicks an individual box in the chart.
+   * Updates the browser URL.
+   */
+  private onSelection = (selection: IndiInfo) => {
+    analyticsEvent('selection_changed');
+    //const location = this.props.location;
+    //const search = queryString.parse(location.search);
+    //search.indi = selection.id;
+    //search.gen = String(selection.generation);
+    //location.search = queryString.stringify(search);
+    //this.props.history.push(location);
+    this.props.onUpdateDeCujus(selection.id)
+  }; 
+
+  private onPrint = () => {
+    analyticsEvent('print');
+    this.chartRef && this.chartRef.print();
+  };
+
+  private showErrorPopup(message: string, otherStateChanges?: Partial<State>) {
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        {
+          showErrorPopup: true,
+          error: message,
+        },
+        otherStateChanges,
+      ),
+    );
+  }
+
+  private onDownloadPdf = async () => {
+    analyticsEvent('download_pdf');
+    try {
+      this.chartRef && (await this.chartRef.downloadPdf());
+    } catch (e) {
+      this.showErrorPopup('Failed to generate PDF file.' +
+      ' Please try with a smaller diagram or download an SVG file.'
+      );
+    }
+  };
+
+  private onDownloadPng = async () => {
+    analyticsEvent('download_png');
+    try {
+      this.chartRef && (await this.chartRef.downloadPng());
+    } catch (e) {
+      this.showErrorPopup('Failed to generate PNG file.' +
+      ' Please try with a smaller diagram or download an SVG file.');
+    }
+  };
+
+  private onDownloadSvg = () => {
+    analyticsEvent('download_svg');
+    this.chartRef && this.chartRef.downloadSvg();
+  };
+
+  private onDismissErrorPopup = () => {
+    this.setState(
+      Object.assign({}, this.state, {
+        showErrorPopup: false,
+      }),
+    );
+  };
+
+  private renderMainArea = () => {
+    
+    this.props.onUpdateMemberList(data)
+
+    switch (this.state.state) {
+      case TreeState.SHOWING_CHART:
+      case TreeState.LOADING_MORE: 
+        return (
+          <div id="content">
+            <ErrorPopup
+              open={this.state.showErrorPopup}
+              message={this.state.error}
+              onDismiss={this.onDismissErrorPopup}
+            />
+            {this.state.state === TreeState.LOADING_MORE ? (
+              <Loader active size="small" className="loading-more" />
+            ) : null}
+            <Chart
+              //data={this.state.data!.chartData}
+              data={data}
+              selection={this.state.selection!}
+              chartType={this.state.chartType}
+              onSelection={this.onSelection}
+              freezeAnimation={this.state.freezeAnimation}
+              ref={(ref) => (this.chartRef = ref)}
+            />
+            {this.state.showSidePanel ? (
+              <Responsive minWidth={768} id="sidePanel">
+                <Details
+                  gedcom={this.state.data!.gedcom}
+                  indi={this.state.selection!.id}
+                />
+              </Responsive>
+            ) : null}
+          </div>
+        );
+
+      case TreeState.ERROR:
+        return <ErrorMessage message={this.state.error!} />;
+
+      case TreeState.INITIAL:
+      case TreeState.LOADING:
+        return <Loader active size="large" />;
+    }
+  };
 
   render() {
-
     return (
-      <div className="overflow-auto">
-        <br></br>
-        <TreeMember
-          {...this.state.memberlist[this.state.rootid]}
-          onAddPartner={this.handleAddPartner}
-          onAddChild={this.handleAddChild}
-          onEdit={this.handleMemberEdit}
-          onUpdateStatus={this.handleUpdateStatus}
-          onUpdateDeCujus={this.props.onUpdateDeCujus}
-          onDelete={this.handleMemberDelete}
-          parentPosition={this.state.position}
+      <>
+       <TopBar
+            {...this.props}
+            deCujus={this.props.deCujus}
+            processSolution={this.props.processSolution}
+            title={"Ordre 1"}
+            data={this.state.data && this.state.data.chartData}
+            allowAllRelativesChart={true}
+            showingChart={
+            this.props.history.location.pathname === '/view' &&
+            (this.state.state === TreeState.SHOWING_CHART ||
+                this.state.state === TreeState.LOADING_MORE)
+            }
+            standalone={this.state.standalone}
+            eventHandlers={{
+            onSelection: this.onSelection,
+            onPrint: this.onPrint,
+            onDownloadPdf: this.onDownloadPdf,
+            onDownloadPng: this.onDownloadPng,
+            onDownloadSvg: this.onDownloadSvg,
+            }}
         />
-        </div>
-      )
+        {this.renderMainArea()}
+      </>
+    );
   }
 }
-
-export default Tree;
